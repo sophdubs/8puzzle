9fceb41935d77175f0eb5347f963c52cde2455cd
Sophie Dubois <sophiemargueritedubois@gmail.com>
2019-12-03 21:14:06 -0800

    Users will now be alerted once they have won the game and the ninth tile will no longer be blank

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const nums = [null, 'pos1', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 const neighbors = {
-    'pos1': ['pos2', 'pos4'],
-    'pos2': ['pos1', 'pos3', 'pos5'],
-    'pos3': ['pos2', 'pos6'],
-    'pos4': ['pos1', 'pos5', 'pos7'],
-    'pos5': ['pos2', 'pos4', 'pos6', 'pos8'],
-    'pos6': ['pos3', 'pos5', 'pos9'],
-    'pos7': ['pos4', 'pos8'],
-    'pos8': ['pos5', 'pos7', 'pos9'],
-    'pos9': ['pos6', 'pos8']
+    'one': ['two', 'four'],
+    'two': ['one', 'three', 'five'],
+    'three': ['two', 'six'],
+    'four': ['one', 'five', 'seven'],
+    'five': ['two', 'four', 'six', 'eight'],
+    'six': ['three', 'five', 'nine'],
+    'seven': ['four', 'eight'],
+    'eight': ['five', 'seven', 'nine'],
+    'nine': ['six', 'eight']
 };
 
 let tiles = document.querySelectorAll('.tile');
@@ -34,46 +34,24 @@ function swap(tile, blank) {
     let temp = tile.dataset.pos;
     tile.dataset.pos = blank.dataset.pos;
     blank.dataset.pos = temp;
-    updatePuzzle();
+    tile.style.gridArea = tile.dataset.pos;
+    blank.style.gridArea = blank.dataset.pos;
+    checkWin();
 }
 
-
-function updatePuzzle(){
+function checkWin() {
+    let win = true;
     tiles.forEach(tile => {
-        let pos = tile.dataset.pos;
-        switch (true) {
-            case pos === 'pos1':
-                tile.style.gridArea = "pos1";
-                break;
-            case pos === 'pos2':
-                tile.style.gridArea = "pos2";
-                break;
-            case pos === 'pos3':
-                tile.style.gridArea = "pos3";
-                break;   
-            case pos === 'pos4':
-                tile.style.gridArea = "pos4";
-                break;
-            case pos === 'pos5':
-                tile.style.gridArea = "pos5";
-                break;
-            case pos === 'pos6':
-                tile.style.gridArea = "pos6";
-                break;   
-            case pos === 'pos7':
-                tile.style.gridArea = "pos7";
-                break;
-            case pos === 'pos8':
-                tile.style.gridArea = "pos8";
-                break;
-            case pos === 'pos9':
-                tile.style.gridArea = "pos9";
-                break;
+        if (tile.dataset.pos !== tile.id) {
+            win = false;
         }
     })
-    console.log('done');
+    if (win) {
+        console.log('You Win!');
+        blankTile.classList.add('win');
+    }
 }
 
 
 
-updatePuzzle();
+
