3cb6014e92da1823d01a5f2eaab4a30bf776e6fb
Sophie Dubois <sophiemargueritedubois@gmail.com>
2019-12-10 18:31:08 -0800

    Changed the getHScore and countManDistance to work from the getTileState state array as opposed to working with the actual tiles for easier tile manipulation later

--- solver.js
+++ solver.js
@@ -1,54 +1,31 @@
 const stringToNum = [null, 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 
+
+//Tested and working
 function getTileState() {
     let state = [];
     tiles.forEach(tile => {
         state[stringToNum.indexOf(tile.dataset.pos)] = tile.id;
     });
-    console.log(state);
+    return state;
 }
 
-function solve() {
-    let gScore = 0;
-}
-
-// function swap(tile, blank, shuffling=false) {
-//     //Swapping the data-pos values to keep track of which tiles are where. 
-//     let temp = tile.dataset.pos;
-//     tile.dataset.pos = blank.dataset.pos;
-//     blank.dataset.pos = temp;
-//     //Swapping their actual positions in the grid
-//     tile.style.gridArea = tile.dataset.pos;
-//     blank.style.gridArea = blank.dataset.pos;
-
-//     //This is just to avoid any issues while shuffling the puzzle at beginning of game. 
-//     if (!shuffling) {
-//         movesMade += 1;
-//         movesCounter.innerHTML = movesMade;
-//         checkWin();
-//     }
-// }
-
-
-
-
-
-function getHScore() {
-    hScoreSum = 0;
-    tiles.forEach(tile => {
-        hScore = countManDistance(tile);
+//Tested and working
+function getHScore(state) {
+    let hScoreSum = 0;
+    state.forEach((tile, pos) => {
+        let hScore = countManDistance(tile, pos);
         hScoreSum += hScore;
-    })
+    });
     return hScoreSum;
 }
 
 //Tested and working
-function countManDistance(tile){
-    if (tile.id === tile.dataset.pos || tile.id === "nine") {
+function countManDistance(tile, pos){
+    if (tile === stringToNum[pos] || tile === "nine" || tile === null) {
         return 0;
     } else {
-        let pos = stringToNum.indexOf(tile.dataset.pos);
-        let id = stringToNum.indexOf(tile.id);
+        let id = stringToNum.indexOf(tile);
         let vertical = Math.floor(Math.abs(pos - id)/3);
         let horizontal = Math.abs(pos - id) % 3;
         return vertical + horizontal;
