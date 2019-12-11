885db27a1d8569ab39e804799ec0c1feb5a0b9e2
Sophie Dubois <sophiemargueritedubois@gmail.com>
2019-12-10 18:14:08 -0800

    Creating an array showing the current state of the tiles

--- solver.js
+++ solver.js
@@ -1,25 +1,49 @@
 const stringToNum = [null, 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 
-function solve() {
-    
+function getTileState() {
+    let state = [];
+    tiles.forEach(tile => {
+        state[stringToNum.indexOf(tile.dataset.pos)] = tile.id;
+    });
+    console.log(state);
 }
 
+function solve() {
+    let gScore = 0;
+}
+
+// function swap(tile, blank, shuffling=false) {
+//     //Swapping the data-pos values to keep track of which tiles are where. 
+//     let temp = tile.dataset.pos;
+//     tile.dataset.pos = blank.dataset.pos;
+//     blank.dataset.pos = temp;
+//     //Swapping their actual positions in the grid
+//     tile.style.gridArea = tile.dataset.pos;
+//     blank.style.gridArea = blank.dataset.pos;
+
+//     //This is just to avoid any issues while shuffling the puzzle at beginning of game. 
+//     if (!shuffling) {
+//         movesMade += 1;
+//         movesCounter.innerHTML = movesMade;
+//         checkWin();
+//     }
+// }
 
 
 
 
 
-
-function getTileState() {
+function getHScore() {
     hScoreSum = 0;
     tiles.forEach(tile => {
-        hScore = countMovesToPos(tile);
-        hsScoreSum += hScore;
+        hScore = countManDistance(tile);
+        hScoreSum += hScore;
     })
+    return hScoreSum;
 }
 
 //Tested and working
-function countMovesToPos(tile){
+function countManDistance(tile){
     if (tile.id === tile.dataset.pos || tile.id === "nine") {
         return 0;
     } else {
