32166504f15116bc6b30c3bcab62c12083796479
Sophie Dubois <sophiemargueritedubois@gmail.com>
2019-12-18 19:27:43 -0800

    I think the algorithm is working. Need to clean up and test a bit more to make sure

--- solver.js
+++ solver.js
@@ -1,7 +1,26 @@
+// Allows easy conversion between string and int using indexOf('num');
 const stringToNum = [null, 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 
+//  This function builds the starting node and kicks off the A* algorithm
+function solvePuzzle() {
+    const state = getTileState();
+    const neighbors = findBlankNeighbors(state);
+    const hScore = getHScore(state);
+    const gScore = 0;
+    const fScore = gScore + hScore;
 
-//Tested and working
+    let startNode = {
+        fScore,
+        gScore, 
+        hScore, 
+        state,
+        neighbors,
+        prev: null
+    }
+    return aStar(startNode);
+}
+
+// Returns an array reflecting the current state of the tiles
 function getTileState() {
     let state = [];
     tiles.forEach(tile => {
@@ -10,32 +29,29 @@ function getTileState() {
     return state;
 }
 
+// Returns an array reflecting the state if blank tile swaps with provided neighbor
 function findSwappedState(neighbor, state){
     currState = state.slice();
-   
     let blankIndex = currState.indexOf('nine');
     let neighborIndex = stringToNum.indexOf(neighbor);
     let neighborText = currState[neighborIndex];
-   
     currState[blankIndex] = neighborText;
     currState[neighborIndex] = 'nine';
-
     return currState;
-    
 }
 
-//Tested and working
+// 
 function getHScore(state) {
     let hScoreSum = 0;
     state.forEach((tile, pos) => {
-        let hScore = countManDistance(tile, pos);
+        let hScore = countManhattenDistance(tile, pos);
         hScoreSum += hScore;
     });
     return hScoreSum;
 }
 
 //Tested and working
-function countManDistance(tile, pos){
+function countManhattenDistance(tile, pos){
     if (tile === stringToNum[pos] || tile === "nine" || tile === null) {
         return 0;
     } else {
@@ -58,9 +74,10 @@ function findBlankNeighbors(state) {
 function reconstructPath(current) {
     let path = [current];
     while (current.prev) {
-        path.unshift(current.prev);
+        path.push(current.prev);
         current = current.prev;
     }
+    console.log('path', path);
     return path;
 }
 
@@ -77,7 +94,7 @@ function reconstructPath(current) {
 //     prev: c
 // }
 
-function aStar(startNode, endState) {
+function aStar(startNode) {
     let openSet = [startNode];
     let closedSet = [];
 
@@ -86,20 +103,54 @@ function aStar(startNode, endState) {
         let index = findSmallestF(openSet);
         // 2. Remove it from openSet and call it curr
         let curr = openSet.splice(index, 1)[0];
+        console.log('OpenSet', openSet);
+        console.log(curr);
+        // console.log('current open set', openSet);
+        // console.log('open set length', openSet.length);
+        // console.log('current curr', curr);
         // 3. Check to see if curr.state = solution state. If so, return reconstruct path with current node. 
-        if (curr.state === endState) {
-            console.log('here');
+        console.log('here no issues');
+        if (isSolvedState(curr.state)) {
+            console.log('SOLVED');
             return reconstructPath(curr);
         }
-        // 4. Process eacg neighbor
+        // 4. Process each neighbor
         let neighbors = curr.neighbors.map(processNeighbors, curr);
-                //Check to see if we have seen it before
-             
-                //If so, set its parent to be curr
-                //Set its gscore to be curr.gscore + 1
-            //If not, it means we havent seen it yet, process it normally
-        // 5. Add all neighbor nodes to openSet. 
-        //
+        console.log(neighbors);
+        //Check to see if we have seen it before
+        for (let i = 0; i < neighbors.length; i++) {
+            let seen = checkIfSeen(neighbors[i], closedSet);
+            let open = checkIfSeen(neighbors[i], openSet);
+            if (!seen && !open) {
+                // console.log(`pushed ${i}`);
+                openSet.push(neighbors[i]); 
+            }
+        }
+        // console.log('openSet',openSet);
+        closedSet.push(curr);
+        console.log('closedSet', closedSet);
+    }
+}
+
+function isSolvedState(currentState) {
+    const solvedState = [null, 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    for (let i = 1; i < currentState.length; i++) {
+        if (currentState[i] !== solvedState[i]) {
+            return false;
+            
+        }
+    }
+    console.log('returning true');
+    return true;
+}
+
+    function checkIfSeen(neighbor, closedSet) {
+        for (let i = 0; i < closedSet.length; i++) {
+            if (closedSet[i].state === neighbor.state) {
+                true
+            }
+        }
+        return false;
     }
 
     //Tomorrow will swap this for priority queue instead to avoid linear time search everytime to find the miminum fScore node. 
@@ -108,6 +159,7 @@ function aStar(startNode, endState) {
         let index = null;
         arr.forEach((node, i) => {
             if (node.fScore < f) {
+                f = node.fScore;
                 index = i;
             }
         });
@@ -131,36 +183,6 @@ function aStar(startNode, endState) {
            prev: this
         }
     }
-}
 
-let startNode = {
-    fScore: null,
-    gScore: 0,
-    hScore: null,
-    state: [],
-    neighbors: [],
-    prev: null
-}
-
-function solvePuzzle() {
-    const state = getTileState();
-    const neighbors = findBlankNeighbors(state);
-    const hScore = getHScore(state);
-    const gScore = 0;
-    const fScore = gScore + hScore;
-
-    const endState = [null, 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-
-    let startNode = {
-        fScore,
-        gScore, 
-        hScore, 
-        state,
-        neighbors,
-        prev: null
-    }
-
-    return aStar(startNode, endState);
-}
 
 
